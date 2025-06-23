446cda7faab67b0e01295a660e98123a79a474d2
Ibne-suny <mdivnesuny2021@gmail.com>
2025-06-23 21:28:02 +0600

    ****

---
+++ pattern_1.java
@@ -0,0 +1,25 @@
+package Nine_best_parttern_question;
+
+public class pattern_1 {
+  public static void main(String[] args) {
+    /*
+       *****
+       *****
+       *****
+       *****
+     */
+
+
+     int n=5;
+     int m=4;
+
+     for(int i=1;i<=4;i++){
+      for(int j=1;j<=5;j++){
+        System.out.print("*");
+
+      }
+      System.out.println();
+     }
+  }
+    
+}
