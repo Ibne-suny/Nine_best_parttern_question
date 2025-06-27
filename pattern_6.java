1459618754677be90362de368d208717166cb99d
Ibne-suny <mdivnesuny2021@gmail.com>
2025-06-27 16:14:38 +0600

    Half pyramid with number

---
+++ pattern_6.java
@@ -0,0 +1,15 @@
+package Nine_best_parttern_question;
+
+public class pattern_6 {
+    public static void main(String[] args) {
+        int n=4;
+
+
+        for(int i=1; i<=n;i++){
+            for(int j=1;j<=i;j++){
+                System.out.print(j+" ");
+            }
+            System.out.println();
+        }
+    }
+}
